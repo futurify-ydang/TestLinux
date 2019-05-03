f9cbebb9d9d07da73b60d51f30459559960e402e
Đặng Huỳnh Đạt Ý <daty.danghuynh20497@gmail.com>
2019-05-03 18:00:48 +0700

    Create NET Core project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace PathTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Path.Combine(Environment.CurrentDirectory,"Data"));
+        }
+    }
+}
